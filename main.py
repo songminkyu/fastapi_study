31d0088e6904403aa7df511c9a22712a6850b869
songminkyu <rabbircarrot@naver.com>
2024-12-29 15:27:09 +0900

    add feature : Add GET version info api

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/version")
+async def say_version():
+    return {"version": "Hello ver : 12.0.0.1"}
