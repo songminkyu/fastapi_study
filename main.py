8cf8d8181acc1593894b1c571c3a78aa1c15d3d2
songminkyu <rabbircarrot@naver.com>
2025-02-01 15:26:19 +0900

    pydantic api 조합 테스트 코드 추가

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel, Field
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
@@ -14,3 +13,13 @@ async def say_hello(name: str):
 @app.get("/version")
 async def say_version():
     return {"version": "Hello ver : 12.0.0.1"}
+
+class DataInput(BaseModel):
+    name: str
+
+class PredicOutput(BaseModel):
+    prob: float
+    prediction: int
+@app.post("/pydantic", response_model=PredicOutput)
+def pydantic_post(data_request:DataInput):
+    return{'prob': 0.1, 'prediction': 5}
--- test/BaseSetting_example.py
+++ test/BaseSetting_example.py
@@ -1,11 +1,37 @@
 from typing import Optional, List, Union
+from pydantic import Field, validator, ValidationError, ValidationInfo, field_validator, BaseModel
 from pydantic_settings import BaseSettings,SettingsConfigDict
 
 # https://docs.pydantic.dev/latest/concepts/pydantic_settings/
 class DBConfig(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env_ex", env_file_encoding="utf-8")
-    db_host: str
-    db_port: int
+    db_host: str = Field(default="localhost")
+    db_port: int = Field(default=3306)
 
-conf = DBConfig()
-print(conf)
+    @field_validator('db_host', mode='before')
+    def check_host(cls, host_input):
+        if host_input == 'localhost':
+            return '127.0.0.1'
+        return host_input
+
+    @field_validator('db_port')
+    def check_port(cls, port_input):
+        if port_input not in [12345, 3306]:
+            raise ValueError("port error")
+        return port_input
+
+class ProjectConfig(BaseModel):
+    project_name: str = 'likelihood'
+    db_info: DBConfig = DBConfig()
+
+config_data={
+    "project_name":"likelihood project",
+    "db_info":{
+        'db_host':'localhost',
+        'db_port':3306
+    }
+}
+
+project_config=ProjectConfig(**config_data)
+print(project_config)
+print(project_config.db_info)
